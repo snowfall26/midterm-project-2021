b735942b2ba07dbc3d44e150bd44092d2c07d16a
snowfall26 <95094062+snowfall26@users.noreply.github.com>
2021-12-22 15:19:52 -0500

    Update global energy consumption choropleth map (simple).py

--- global energy consumption choropleth map (simple).py
+++ global energy consumption choropleth map (simple).py
@@ -2,16 +2,7 @@
 INTRO TO CS MIDTERM PROJECT: ANIMATED CHOROPLETH MAP
 by: yi-an liao
 due date: january 3, 2022
-
-data cleaning process:
-1. delete rows without iso-3 country codes to get rid of regional and continent data.
-2. delete rows that have data earlier than 1980 or later than 2015.
-3. delete all data from years that aren't a decade marker or half a decade.
-4. reset index starting from 0 afterwards because why not and it looks better.
-5. end product: data is only from countries and the onlyyears i have data for are 1980, 1985, 
-1990,1995, 2000, 2005, 2010, and 2015.
 """
-
 
 # installed libraries
 '''
